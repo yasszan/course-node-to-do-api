2547eb3b8b6d25598424649b43f59f78d12229b5
Yasser Zantour <yasserzantour@Yassers-MacBook-Pro.local>
2016-12-27 16:25:46 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {User}= require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
-
+//POST//
 app.post('/todos',(req, res) => {
  var todo = new Todo ({
      text: req.body.text
@@ -18,6 +18,15 @@ app.post('/todos',(req, res) => {
      res.status(400).send(e);
  });
 });
+//GET//
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos)=> {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
