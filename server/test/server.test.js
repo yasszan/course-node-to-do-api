3d5e478a96ee4f7052ebba158e8060d4e9d310f0
Yasser Zantour <yasserzantour@Yassers-MacBook-Pro.local>
2016-12-28 15:34:15 +0100

    Test the DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -9,12 +9,13 @@ const todos = [{
     _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
 beforeEach((done) => {
     Todo.remove({}).then(()=> {
-      Todo.insertMany(todos);
+     return Todo.insertMany(todos);
     }).then(()=> done());
 });
 
@@ -92,5 +93,38 @@ request(app)
    .end(done);    
 
 });
+});
+describe('DELETE /todos/:id', () => {
+it('should remove a todo', (done) => {
+  var hexId = todos[1]._id.toHexString();
+  request(app)
+  .delete(`/todos/${hexId}`)
+  .expect(200)
+  .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err, res) => {
+        if(err) {
+            return done(err);
+        }
+        Todo.findById(hexId).then((todo) => {
+           expect(todo).toNotExist();
+           done();
+        }).catch((e) => done(e));
+    });
 
+});
+it('should return 404 if todo not found', (done)=> {
+  var hexId = new ObjectID().toHexString();
+  request(app)
+   .delete(`/todos/${hexId}`)
+   .expect(404)
+   .end(done);
+});
+it('should return 404 if object id is invalid', (done)=>{
+request(app)
+   .delete('/todos/123abc')
+   .expect(404)
+   .end(done);  
+});
 });
